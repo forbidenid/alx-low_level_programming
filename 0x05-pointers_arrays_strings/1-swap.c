f2ada46555e8517c1277f40e0e3f6e26d7ff0936
root-codes <muriithidennis340@gmail.com>
2023-06-26 06:45:48 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap - swaps the value of two integers
+ * @a: first int
+ * @b: second int
+ * Return: 0 Success
+ */
+void swap_int(int *a , int *b)
+{
+	int x;
+	x = *a;
+	*a = *b;
+	*b = x;
+}
