145227e9d147c98e94da86bfe440589110aa9276
root-codes <muriithidennis340@gmail.com>
2023-06-26 07:09:56 -0700

    lengts of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - the function of a string
+ * @s: check char
+ * Return: 0 Success
+ */
+void _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s++)
+		a++;
+	return (a);
+}
