c8e6c2dddb8c0f06464d474ac906bb0b63a586c3
root-codes <muriithidennis340@gmail.com>
2023-07-03 07:14:21 -0700

    string char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - returns a point to the first occurance of a character in a string.
+ * @s: string to search for the character in.
+ * @c: char to the search for.
+ *
+ * Return: pointer to char or else NULL.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((s + i));
+		i++;
+	}
+	if (c == '\0')
+		return ((s + i));
+	return (NULL);
+}
