8dc6c6891c92406ed9b2ee9e3ada9d9d9e02cadc
root-codes <muriithidennis340@gmail.com>
2023-07-05 08:14:27 -0700

    sqrt damn

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,7 +18,7 @@ int _sqrt_recursion(int n)
 		return (n);
 	if (!(n % 2))
 	{
-		if (n / 2 == 2 || n == 2)	
+		if (n / 2 == 2 || n == 2)
 			return (2);
 		sqrt = _sqrt_recursion(n / 2);
 		if (n / sqrt == sqrt)
