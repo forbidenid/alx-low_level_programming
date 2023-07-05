47124a5866ba213aaba2de7181c4e9fe7903ae58
root-codes <muriithidennis340@gmail.com>
2023-07-05 06:26:55 -0700

    pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the value of x raiset to the power of y
+ * @x: value to be manipulated
+ * @s: the value manipulator
+ * Return: Sun INT
+ * root-codes
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (!y)
+		return (1);
+	return (x * _pow_recursion(x, --y));
+}
