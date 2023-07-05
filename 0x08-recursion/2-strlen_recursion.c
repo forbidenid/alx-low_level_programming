5ac52960a1d0f2d5e9fba3c13ff52de8a183367c
root-codes <muriithidennis340@gmail.com>
2023-07-05 05:07:01 -0700

    strlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a sdtring
+ * @s: string to check
+ * Return: the ength of a string as INT
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+		return (1 + _strlen_recursion((s + 1));
+	return (0);
+}
