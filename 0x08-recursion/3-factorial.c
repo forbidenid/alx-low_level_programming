63e864f295c6775f2783fe812ac89722a17183bf
root-codes <muriithidennis340@gmail.com>
2023-07-05 06:14:16 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - description
+ * @n: The number to find the factorial for.
+ * root-codes
+ * Return: Factorial INT
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return(-1);
+	else if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}
