a41fee32ea71cb4f65bb4c4a7db47367ecbd46a1
root-codes <muriithidennis340@gmail.com>
2023-07-05 04:21:45 -0700

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: string to print in reverse
+ * Return: void
+ */
+void _ptint_rev_recursion(char *s)
+{
+	if (!*s)
+		return;
+	_print_rev_recursion((s + 1));
+	_putchar(*s);
+}
