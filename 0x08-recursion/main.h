09bce16bb3f47bc3546b3c566aa035bc70ffab94
root-codes <muriithidennis340@gmail.com>
2023-07-05 04:01:46 -0700

    update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
-
 int _puchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -12,6 +6,3 @@ int factorial(ibt n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is _prime_number(int n);
-
-
-#endif
