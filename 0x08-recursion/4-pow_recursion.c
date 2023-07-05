02ddb2a28dc4d11a66507c43ff6a935c0354852d
root-codes <muriithidennis340@gmail.com>
2023-07-05 06:49:12 -0700

    pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - function that returns the 
  * value of x raises to the power of y
  * @x: value to be manipulated
- * @s: the value manipulator
+ * @s: manipulator
  * Return: Sum as Int
  */
 int _pow_recursion(int x, int y)
