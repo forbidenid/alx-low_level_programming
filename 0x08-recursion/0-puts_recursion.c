ad394aa80747e1e616897dc860e61bb49736e8b4
root-codes <muriithidennis340@gmail.com>
2023-07-05 04:14:57 -0700

    main.h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,15 +5,13 @@
  * Return: void
  */
 
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
-if (*s)
-{
+	if (!*s)
+	{
+		_putchar('\n');
+		return
+	}
 	_putchar(*s);
-	_puts_recursion(s + 1);
-}
-else
-}
-	_putchar("\n");
-{
+	_puts_recursion((s + 1));
 }
