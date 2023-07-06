3f1a9a5f59e4ed15e6adb934c90b9cc43b61fa3c
root-codes <muriithidennis340@gmail.com>
2023-07-06 06:44:54 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,5 +16,18 @@ int str_checker(char *s1, char *s2, int i, int j)
 	if (s1[i] == "\0" && s2[j] == "*")
 		return (str_checker(s1, s2, i, ++j));
 	if (s2[j] == "*")
+		return (str_checker(s1, s2, ++i j) || str_checker(s1, s2, i, ++j));
+	return (0);
 
+}
+
+/**
+ * wildcmp - check if the string is identical
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 if similar
+ */
+int wildcmp(char *s1, char *s2)
+{
+	return (str_checker(s1, s2, 0, 0);
 }
