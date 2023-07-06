b5a3885d14502c216f54d886ff588df9eedb287d
Muriithi Dennis <108466862+root-codes@users.noreply.github.com>
2023-07-06 17:54:15 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,33 +1,30 @@
 #include "main.h"
-/**
- * str_checker - check if two strings are similar
- * @s1: string 1
- * @s2: string 2
- * @i: left index
- * @j: joker
- * Return: 1 else 0
- */
-int str_checker(char *s1, char *s2, int i, int j)
-{
-	if (s2[i] == '\0' && s2[j] == '\0')
-		return (1);
-	if (s1[i] == s2[j])
-		return (str_checker(s1, s2, ++i, ++j));
-	if (s1[i] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, i, ++j));
-	if (s2[j] == '*')
-		return (str_checker(s1, s2, ++i, j) || str_checker(s1, s2, i, ++j));
-	return (0);
-
-}
 
 /**
- * wildcmp - check if the string is identical
- * @s1: string 1
- * @s2: string 2
- * Return: 1 if similar
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
+
 int wildcmp(char *s1, char *s2)
 {
-	return (str_checker(s1, s2, 0, 0));
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && *s2 == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (*s2 == '\0');
+}
+
+if (*s2 == '*')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
 }
