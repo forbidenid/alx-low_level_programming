4aae4d8f50cff607533047d08aeace19dc2ac42d
root-codes <muriithidennis340@gmail.com>
2023-07-06 07:03:12 -0700

    dev

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,13 +9,13 @@
  */
 int str_checker(char *s1, char *s2, int i, int j)
 {
-	if (s2[i] == "\0" && s2[j] == "\0")
+	if (s2[i] == '\0' && s2[j] == '\0')
 		return (1);
 	if (s1[i] == s2[j])
 		return (str_checker(s1, s2, ++i, ++j));
-	if (s1[i] == "\0" && s2[j] == "*")
+	if (s1[i] == '\0' && s2[j] == '*')
 		return (str_checker(s1, s2, i, ++j));
-	if (s2[j] == "*")
+	if (s2[j] == '*')
 		return (str_checker(s1, s2, ++i j) || str_checker(s1, s2, i, ++j));
 	return (0);
 
