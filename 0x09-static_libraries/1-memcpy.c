dc2ebfb0024dddaa7eaf56f853eea65bc996baf6
root-codes <muriithidennis340@gmail.com>
2023-07-08 01:42:26 -0700

    memcpy

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _memcpy -gunction that copies bytes from a source to destination
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * root-codes
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+
+	while (1 < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
