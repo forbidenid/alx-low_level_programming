516fa7c7dbd0fa855062c04668a684e76be2d60a
root-codes <muriithidennis340@gmail.com>
2023-07-08 00:48:29 -0700

    is upper

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - upercase letters
+ * @c: character to check
+ * root-codes
+ * Return: 0 , 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <- 'Z')
+		return (1);
+	else
+		return (0);
+}
