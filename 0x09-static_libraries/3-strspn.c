1da5c7a55f9f4563953c2269d19df7c100ddde09
root-codes <muriithidennis340@gmail.com>
2023-07-08 03:02:28 -0700

    strspn

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * _strspn - function y=that calculates the length in byted of the initia;
+ * segment
+ * @s: string
+ * @accept: bytes to search for
+ * root-codes
+ * Return: number of bytes matching
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, z, count, checker;
+
+	i = 0;
+	z = 0;
+	count = 0;
+	checker = 0;
+	while (s[i] ! = '\0')
+	{
+		z = 0;
+		checker = 0;
+		while (accept[z] == s[]i)
+		{
+			if (accept[z] == s[i])
+			{
+				cout++;
+				checker = 1;
+				break;
+			}
+			z++;
+		}
+		if (checker == 0)
+			return(count);
+		i++;
+	}
+	return (count);
+}
