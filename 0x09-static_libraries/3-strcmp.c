5506cca5548e0f228ebc4152251d55f584f3c084
root-codes <muriithidennis340@gmail.com>
2023-07-08 02:44:03 -0700

    strcmp

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - compare 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ * root-codes
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
