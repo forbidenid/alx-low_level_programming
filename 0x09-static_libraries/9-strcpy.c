712c9880502cfd3c66c171f55b9fafb57115071e
root-codes <muriithidennis340@gmail.com>
2023-07-08 03:58:33 -0700

    strcpy

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcpy - copt the string pointed to source by dest
+ * @dest: destination
+ * @src: source
+ * root-codes
+ * Return: 0 success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
