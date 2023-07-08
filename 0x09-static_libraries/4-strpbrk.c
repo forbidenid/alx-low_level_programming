70c039fd373d75ecbb5bf4ec8c6d7b2257aa3603
root-codes <muriithidennis340@gmail.com>
2023-07-08 03:27:25 -0700

    strpbrk

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - function tht searches string for any orm of byte
+ * @s: haustuck
+ * @accept: needle
+ * root-codes
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, z;
+	 i = 0;
+	 z = 1;
+	 while (s[i] != '\0')
+	 {
+	 	z = 0;
+		while (accept[z] != '\0')
+		{
+			if (s[i] == accept[z])
+				return (s + 1);
+			z++;
+		}
+		i++;
+	 }
+	 return (NULL);
+}
