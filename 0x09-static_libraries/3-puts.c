d19ea8b2a4ba6338de3a027beb1b2a0a02f01471
root-codes <muriithidennis340@gmail.com>
2023-07-08 02:33:03 -0700

    puts

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - put a string followed by  a ew line
+ * @str: string
+ * root-codes
+ * Return: length of the string
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n')'
+}
