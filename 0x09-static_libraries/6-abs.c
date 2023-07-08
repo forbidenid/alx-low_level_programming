d99a5ee74899e674c281ee7b81bbcd207859bf7e
root-codes <muriithidennis340@gmail.com>
2023-07-08 03:52:03 -0700

    abs

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computers the absolute value of an integer
+ * @n: integer
+ * root-codes
+ * Return: abolute value of an integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
