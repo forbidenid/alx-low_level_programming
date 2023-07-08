bc43c87e47981f2399c9acbd97a35fe66370fa20
root-codes <muriithidennis340@gmail.com>
2023-07-08 05:12:08 -0700

    strincht

--- 0x09-static_libraries/2-strchar.c
+++ 0x09-static_libraries/2-strchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * _strchr - returns a point to the first app of the cher str
- * @s: string
- * @c: char
- * root-codes
- * Return: pointer to char
+ * _strchr - returns a point to the first occurance of a character in a string.
+ * @s: string to search for the character in.
+ * @c: char to the search for.
+ *
+ * Return: pointer to char or else NULL.
  */
-char *_strchr(char *s, char *c)
+char *_strchr(char *s, char c)
 {
 	int i;
 
 	i = 0;
-	while (s[1] != '\0')
+	while (s[i] != '\0')
 	{
 		if (s[i] == c)
-			return (s + i);
+			return ((s + i));
 		i++;
 	}
 	if (c == '\0')
-		return (s + i);
+		return ((s + i));
 	return (NULL);
 }
