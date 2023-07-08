7f236f85dcef4e22f7fa8f23c8309e0538f7cf49
root-codes <muriithidennis340@gmail.com>
2023-07-08 01:09:56 -0700

    coding on weekend

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - function that fills memory with constant byte
+ * @b: constant byte
+ * @n: number of bytes
+ * @s: pointer to memory
+ * root-codes
+ * Return a pointer to memory
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
