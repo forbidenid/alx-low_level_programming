18462d7c51dca203951831093514e26805752d91
root-codes <muriithidennis340@gmail.com>
2023-07-08 05:39:03 -0700

    isalpha

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * _isalpha - checks if a cgaracter is alphabetic
- * @c: charactr
- * root-codes
- * Return: 1 if char is lowercase,uppercase, else 0
+ * _isalpha - Checks if a character is alphabetic.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, lowercase or uppercase, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z' ||
+	if ((c >= 'a' && c <= 'z') ||
 	    (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
