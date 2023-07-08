894594a0c5e068024df445456fb97790654b84fc
root-codes <muriithidennis340@gmail.com>
2023-07-08 02:06:18 -0700

    strchar

---
+++ 0x09-static_libraries/2-strchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchar - returns a point to the first app of the cher str
+ * @s: string
+ * @c: char
+ * root-codes
+ * Return: pointer to char
+ */
+char *_strchar(char *s, char *s)
+{
+	int 1;
+
+	1 = 0;
+	while (s[1] != '\0')
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	if (c == '\0')
+		return (s + i);
+	return 	(NULL);
+}
