f271e1201504066a8970bdde0ae08828406b8221
root-codes <muriithidennis340@gmail.com>
2023-07-09 04:07:26 -0700

    till wen

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strcpy - function that copies a string
+ * _strncpy - function that copies a string
  * @src: source
  * @dest: destination
  * root-codes
  * Return: pointer to dest
  */
-char *_strcpy(char *dest, char *src)
+char *_strncpy(char *dest, char *src)
 {
 	int a;
 
