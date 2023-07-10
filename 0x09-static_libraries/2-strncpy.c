5ea9cdfc4dea948e2f632f177e2eabfea08a838b
root-codes <muriithidennis340@gmail.com>
2023-07-09 23:51:37 -0700

    still wstuck

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,17 +1,26 @@
 #include "main.h"
-/**
- * _strncpy - function that copies a string
- * @src: source
- * @dest: destination
- * root-codes
- * Return: pointer to dest
- */
-char *_strncpy(char *dest, char *src)
-{
-	int a;
 
-	for (a = 0; src[a] != '\0'; a++)
-		dest[a] = src[a];
-	dest[a] = '\0';
+/**
+ *_strncpy - function that copies a string
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+	for (i = 0; i < n && *(src + i); i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for (; i < n; i++)
+	{
+	*(dest + i) = '\0';
+	}
 	return (dest);
+
 }
