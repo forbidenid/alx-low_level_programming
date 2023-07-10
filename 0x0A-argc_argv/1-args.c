2a104c63dc411242ce6f84b4594fbe7b9419267c
root-codes <muriithidennis340@gmail.com>
2023-07-10 01:28:11 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the header function
+ * @argc: count
+ * @argv: argument string
+ * root-codes
+ * Return: Null
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc -1);
+	return (argc - 1);
+}
