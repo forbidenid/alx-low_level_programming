316957c2e2081221ec1a2c3f58c1b464027a82ee
root-codes <muriithidennis340@gmail.com>
2023-07-10 01:12:08 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start point.
+ * @argc: size of argv
+ * @argv: name of app
+ * root-codes
+ * Return: Always 0 (Success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", agrv[o]);
+	return (0);
+}
