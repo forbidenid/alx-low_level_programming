42b21ed81e8ef8aee2b94890ebfba0298b4d734d
root-codes <muriithidennis340@gmail.com>
2023-07-11 01:03:36 -0700

    argstostr

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenates all arguments of the progran
+ * @ac: argument
+ * @av: pointer
+ * root-codes
+ * Return: NULL
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, s, size;
+	char *arg;
+
+	size = 0;
+	s = 0;
+
+	if (ac == 0 || ac == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			size++;
+		}
+		size++;
+	}
+	arg = malloc((sizeof(char) * (size + 1)));
+	if (arg == NULL)
+		return (NULL);
+	s = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]);
+		{
+			arg[s] = av[i][j];
+			s++;
+		}
+	}
+	arg[k] = '\0';
+
+	return (arg);
+}
