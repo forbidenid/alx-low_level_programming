a0dad6295e81bb1f14957e76bc401a527a5a2a9a
root-codes <muriithidennis340@gmail.com>
2023-07-11 02:47:47 -0700

    str

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,12 +9,14 @@
 int count_word(char *s)
 {
 	 int flag, c, w;
+
 	 flag = 0;
+
 	 w = 0;
 
-	 for (c = 0; s[c] != '\0'; c++)
-	 {
-	 	if (s[c] == ' ')
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
 			flag = 0;
 		else if (flag == 0)
 		{
