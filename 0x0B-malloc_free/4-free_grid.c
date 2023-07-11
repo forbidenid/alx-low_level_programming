c5326233f9c891e7cd7dae20df05dcedc5dd4383
root-codes <muriithidennis340@gmail.com>
2023-07-11 00:18:25 -0700

    free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the addres of two dimensional grid
+ * @width: width of the grid
+ * root-codes
+ * Return: 0 (Success)
+ */
+void print_grid(int **grid, int width, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
