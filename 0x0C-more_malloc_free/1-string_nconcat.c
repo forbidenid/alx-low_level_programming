312d5510a86d2d10cdeec4249fa633c8889b382f
root-codes <muriithidennis340@gmail.com>
2023-07-13 07:58:18 -0700

    codee commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,8 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
+
 	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
+
 	s[i] = '\0';
 
 	return (s);
