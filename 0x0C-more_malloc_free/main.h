e898318bf0d0fa8154fbfffcbc2e16573bd58344
root-codes <muriithidennis340@gmail.com>
2023-07-13 07:59:40 -0700

    update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,7 +3,7 @@
 
 int *array_range(int min, int max);
 void *_calloc(unsigned int nmemb, unsigned int size);
-char *string_nconcat(char *s1, char s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 
 #endif
