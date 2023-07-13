3e37ef52584ac6785ee3c3adc0533f2c6c4a7efe
root-codes <muriithidennis340@gmail.com>
2023-07-13 07:54:20 -0700

    string conct

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,6 +24,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (!s)
 		return (NULL);
 
+	while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
 	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
 	while (n >= len2 && i < (len1 + len2))
