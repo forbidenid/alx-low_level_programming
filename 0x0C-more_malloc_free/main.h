a04dae366160aaee4addbfb5e2a5f6e42761f221
root-codes <muriithidennis340@gmail.com>
2023-07-13 10:12:55 -0700

    realloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,16 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+
+void _print(char *str, int l);
+char *mul(char n, char *num, int num_index, char *dest, int dest_index);
+int check_for_digits(char **av);
+void init(char *str, int l);
 int _putchar(char c);
-int *array_range(int min, int max);
-void *_calloc(unsigned int nmemb, unsigned int size);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
