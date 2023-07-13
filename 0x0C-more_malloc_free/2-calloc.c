985fd5ffe505680445bc3dbfa1974d41996eaf4c
root-codes <muriithidennis340@gmail.com>
2023-07-13 09:12:14 -0700

    call

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -31,10 +31,13 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
+
 	_memset(ptr, 0, nmemb * size);
+
 	return (ptr);
 }
