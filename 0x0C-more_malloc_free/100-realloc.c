450bd7490905b88c1dc960a0b9c3d0760925adc6
root-codes <muriithidennis340@gmail.com>
2023-07-14 02:58:15 -0700

    real

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,7 +16,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		p = malloc(new_size);
-			return (p);
+		return (p);
 	}
 	else if (new_size == 0)
 	{
