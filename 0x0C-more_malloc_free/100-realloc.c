3cf404c59b3be7da2249d9f33d15a16269256a83
root-codes <muriithidennis340@gmail.com>
2023-07-14 03:08:04 -0700

    reallocate

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,21 +23,15 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
-
 	else if (new_size == old_size)
 		return (ptr);
-
 	p = malloc(new_size);
-
 	if (p == NULL)
 		return (NULL);
-
 	if (new_size > old_size)
-		 n = old_size;
-
+		n = old_size;
 	for (i = 0; i < n; i++)
 		p[i] = oldp[i];
-
 	free(ptr);
 	return (p);
 }
