790396aaa0876c9a27d9d2ed70d5ceb5b7938d97
root-codes <muriithidennis340@gmail.com>
2023-07-17 08:18:08 -0700

    pabe

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,9 @@
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
+	{
 		return;
+	}
 	printf("Name: %\n", d->name ? d->name : "(nill)");
 	printf("Age: %.2f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
