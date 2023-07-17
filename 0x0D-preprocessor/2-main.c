d6879db63e863c4f28be64b72c758e5a5895d787
root-codes <muriithidennis340@gmail.com>
2023-07-17 03:55:12 -0700

    main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * main - the header file
+ * char: charactert that 
+ * root-codes
+ * Return: 0 (Success)
+ */
+int main()
+{
+	char *filename = __FILE__;
+	printf("%s\n", filename);
+	return (0);
+}
