d8c53484b66032689ea3638864fbf92e4b971950
root-codes <muriithidennis340@gmail.com>
2023-07-17 08:09:48 -0700

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - struct
+ * @d: struct
+ * root-codes
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %\n", d->name ? d->name : "(nill)");
+	printf("Age: %.3f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+}
