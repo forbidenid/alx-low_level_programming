56fca8eaf89eff87a2c86765bdf6d8e468b1bb80
root-codes <muriithidennis340@gmail.com>
2023-07-17 07:47:50 -0700

    doggg

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  * @owner: of the dog
  */
 
-void init_dog(struct dog *d, chsr *name, float *age, char *owner)
+void init_dog(struct dog *d, char *name, float *age, char *owner)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
