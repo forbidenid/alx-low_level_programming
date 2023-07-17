2d15c3ea403393978248f8c2eac639b35eec13e2
root-codes <muriithidennis340@gmail.com>
2023-07-17 07:27:39 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+/**
+ * struct dog - description
+ * @name: name of the dog
+ * @age: of the dog
+ * @owner: of the dog
+ * Description: of the dog
+ */
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+}
