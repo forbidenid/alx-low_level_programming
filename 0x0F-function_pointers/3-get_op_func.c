7546563cefaa2989f30983f11efcc66c372dc223
root-codes <muriithidennis340@gmail.com>
2023-07-19 09:14:40 -0700

    hello werld

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,13 +16,14 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i; 
+	int i;
 
 	i = 0;
-	while (ops[i].op != NULL && s[1] == '\0')
+
+	while (ops[i].op)
 	{
-		if (ops[i].op[0] == *s)
-			return ((ops[i].f));
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 		i++;
 	}
 
