a96f17f3c6fc7ced2fae2e5b7ea6fbc0ef3c554a
root-codes <muriithidennis340@gmail.com>
2023-07-19 09:12:53 -0700

    still stuck

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,14 +9,14 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i; 
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i; 
 
 	i = 0;
 	while (ops[i].op != NULL && s[1] == '\0')
