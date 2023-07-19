129dc89e56a93c916b1a2ede32864f000ff38718
root-codes <muriithidennis340@gmail.com>
2023-07-19 02:25:09 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name -function that prints a name
+ * @name: the name
+ * @f: function
+ * @*: pointer
+ * root-codes
+ * Return: Always (Success)
+ */
+
+void print_name(char *name, void (*f)(char *));
+{
+
+}
