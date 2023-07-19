f7f37283f0ea75acb4e5f4f6c7feb2e002562094
root-codes <muriithidennis340@gmail.com>
2023-07-19 07:03:04 -0700

    get op fub=nctions

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_op_func - fumction op
+ * @s: opretator
+ * Return: pointer
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+," op_add},
+		{"*", op_mul},
+		{"%", op_div},
+		{"-", op_sub},
+		{NULL, NULL}
+	    };
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL && s[1] == '\0')
+	{
+		if (ops[i].op[0] == *s)
+			return ((ops[i].f));
+		i++;
+	}
+
+	return (NULL);
+}
+
+
+}
