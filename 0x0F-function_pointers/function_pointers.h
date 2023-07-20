bf99f30ad29f89895b9bc51f718cfb18700b1332
root-codes <muriithidennis340@gmail.com>
2023-07-20 02:56:53 -0700

    debugging

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTION_POINTER_H
 #define _FUNCTION_POINTER_H
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
