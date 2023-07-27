26f952d37b0e5b47950e23b0f9bb72d0f92dbc75
root-codes <muriithidennis340@gmail.com>
2023-07-27 08:54:05 -0700

    print lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_lists - a function that prints all the elements of a list
+ * @h: linked list
+ * root-codes
+ * Return: elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+
+}
